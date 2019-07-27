2edccf0656268d7fcffcadf6a0b9ef0e422218e5
Tiffany Maurath <tiffany.maurath@orange.fr>
2019-07-27 19:48:03 +0200

    Création des méthodes d'authentification + formulaires

--- src/app/auth/signin/signin.component.ts
+++ src/app/auth/signin/signin.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {AuthService} from '../../services/auth.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-signin',
@@ -7,9 +10,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SigninComponent implements OnInit {
 
-  constructor() { }
+    signInForm: FormGroup;
+    errorMessage: string;
 
-  ngOnInit() {
-  }
+    constructor( private authService: AuthService,
+                 private formBuilder: FormBuilder,
+                 private router: Router ) {
+
+    }
+
+    ngOnInit() {
+        this.initForm();
+    }
+
+    initForm() {
+        this.signInForm = this.formBuilder.group({
+            email: ['', [Validators.required, Validators.email]],
+            password: ['', [Validators.required, Validators.pattern(/[0-9a-zA-Z]{6,}/)]]
+        });
+    }
+
+    onSubmit() {
+        const email = this.signInForm.get('email').value;
+        const password = this.signInForm.get('password').value;
+        this.authService.signInUser(email, password).then(
+            () => {
+                this.router.navigate(['/lipstick-list']);
+            },
+            (error) => {
+                this.errorMessage = error;
+            }
+        );
+    }
 
 }
--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { AuthService} from '../../services/auth.service';
+import { FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-signup',
-  templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.scss']
+    selector: 'app-signup',
+    templateUrl: './signup.component.html',
+    styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+    signUpForm: FormGroup;
+    errorMessage: string;
 
-  ngOnInit() {
-  }
+    constructor( private authService: AuthService,
+                 private formBuilder: FormBuilder,
+                 private router: Router ) {
+
+    }
+
+    ngOnInit() {
+        this.initForm();
+    }
+
+    initForm() {
+        this.signUpForm = this.formBuilder.group({
+            email: ['', [Validators.required, Validators.email]],
+            password: ['', [Validators.required, Validators.pattern(/[0-9a-zA-Z]{6,}/)]]
+        });
+    }
+
+    onSubmit() {
+        const email = this.signUpForm.get('email').value;
+        const password = this.signUpForm.get('password').value;
+        this.authService.createNewUser(email, password).then(
+            () => {
+                this.router.navigate(['/lipstick-list']);
+            },
+            (error) => {
+                this.errorMessage = error;
+            }
+        );
+    }
 
 }
