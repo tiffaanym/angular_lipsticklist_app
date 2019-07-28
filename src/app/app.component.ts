eaaf8de24c6c6bf5fe87357d6ae6c5618f7c65d6
Tiffany Maurath <tiffany.maurath@orange.fr>
2019-07-28 20:07:22 +0200

    Ajustement tpl + scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,21 +7,18 @@ import * as firebase from 'firebase';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'LipstickList';
-
-  constructor() {
-      // Your web app's Firebase configuration
-      var firebaseConfig = {
-          apiKey: "AIzaSyBlETnXMPVx0MObCzjyb9iGRgu0p_b0pe8",
-          authDomain: "lipsticklist.firebaseapp.com",
-          databaseURL: "https://lipsticklist.firebaseio.com",
-          projectId: "lipsticklist",
-          storageBucket: "lipsticklist.appspot.com",
-          messagingSenderId: "780574066387",
-          appId: "1:780574066387:web:ed944c75128d7e02"
-      };
-      // Initialize Firebase
-      firebase.initializeApp(firebaseConfig);
-  }
-
+    constructor() {
+        // Your web app's Firebase configuration
+        var firebaseConfig = {
+            apiKey: "AIzaSyBlETnXMPVx0MObCzjyb9iGRgu0p_b0pe8",
+            authDomain: "lipsticklist.firebaseapp.com",
+            databaseURL: "https://lipsticklist.firebaseio.com",
+            projectId: "lipsticklist",
+            storageBucket: "lipsticklist.appspot.com",
+            messagingSenderId: "780574066387",
+            appId: "1:780574066387:web:ed944c75128d7e02"
+        };
+        // Initialize Firebase
+        firebase.initializeApp(firebaseConfig);
+    }
 }
